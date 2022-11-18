9c1f5a2d934255fa2284006a3167f90ea5a67fa1
SheikhMujib <mbasheikhmujib@gmail.com>
2022-11-18 20:40:06 +0600

    Reviews api created

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const serviceCollection = client.db('sabbirPhysio').collection('services');
+        const reviewCollection = client.db('sabbirPhysio').collection('reviews');
 
         app.get('/services', async(req, res) =>{
             const query = {}
@@ -29,6 +30,13 @@ async function run(){
             const query = {_id: ObjectId(id)};
             const service = await serviceCollection.findOne(query);
             res.send(service);
+        })
+
+        //Reviews API
+        app.post('/reviews', async(req, res) =>{
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.send(result);
         })
     }
     finally{}
